7e8a04e9b9164a349b9918b67c07c3e968d164c5
gutwillig <99827107+gutwillig@users.noreply.github.com>
2026-02-13 18:20:13 -0800

    Initial Commit

---
+++ MotionAnchorMac/MotionAnchorMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionAnchorMacApp.swift
+//  MotionAnchorMac
+//
+//  Created by Alex Gutwillig on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct MotionAnchorMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
